86e74d9905d5db6f55791bfb16de591ccd6d47e8
Diego Reyes <diegoreyesco@hotmail.com>
2018-09-04 23:12:29 -0500

    ger y adm contr, views

---
+++ app/Http/Controllers/Application/Gerencia/IngresosCostosController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Application\Gerencia;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IngresosCostosController extends Controller
+{
+    //
+}
