90d0c3fbb1812131fdf035c12e4e474eea141509
negocioizy <negocioizy@gmail.com>
2019-02-16 10:53:49 -0500

    test carga servicio

--- app/Http/Controllers/Application/Sistema/CargaServiciosController.php
+++ app/Http/Controllers/Application/Sistema/CargaServiciosController.php
@@ -72,7 +72,7 @@ class CargaServiciosController extends Controller
 
                     //Verificar si el servicio existe en nuestra bd
                     $id_servicio = $emapData[0];
-
+                    dd($id_servicio,(int)$id_servicio,!DB::table('Servicio')->where('IdServicio',$id_servicio)->exists(),!DB::table('Servicio')->where('IdServicio',(int)$id_servicio)->exists());
                     if(!DB::table('Servicio')->where('IdServicio',$id_servicio)->exists()){
                         
                         date_default_timezone_set('America/Lima');
