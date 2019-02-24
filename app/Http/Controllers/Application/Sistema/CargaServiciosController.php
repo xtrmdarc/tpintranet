1b5120e9c4a0577805f7cc4f81795562c2f3bdf6
negocioizy <negocioizy@gmail.com>
2019-02-24 11:14:19 -0500

    test carga

--- app/Http/Controllers/Application/Sistema/CargaServiciosController.php
+++ app/Http/Controllers/Application/Sistema/CargaServiciosController.php
@@ -308,7 +308,7 @@ class CargaServiciosController extends Controller
                         
                         $servicios_val_arr = [
                             'IdServicio' => $id_servicio,
-                            'FechaServicio' => $fechaServicio,
+                            //'FechaServicio' => '',
                             'HoraProgramada' => $horaProgramada,
                             'MontContado' => $montContado,
                             'MontTotalCredito' => $montTotalCred,
@@ -319,7 +319,7 @@ class CargaServiciosController extends Controller
                             'MontPeaje'=> $MontPeaje,
                             'MontEspera' => $MontEspera,
                             'TiempoEspera' => $TiempoEspera,
-                            'FechaRegistroServicio' => $FechaRegistroServicio,
+                            //'FechaRegistroServicio' => $FechaRegistroServicio,
                             'HoraLlegadaVehiculo'=> $HoraLlegadaVehiculo,
                             'HoraInicioServicio'=> $HoraInicioServicio,
                             'HoraFinServicio' => $HoraFinServicio,
@@ -584,7 +584,7 @@ class CargaServiciosController extends Controller
                         
                         $servicios_val_arr = [
                             'IdServicio' => $id_servicio,
-                            'FechaServicio' => '',
+                            'FechaServicio' => $fechaServicio,
                             'HoraProgramada' => $horaProgramada,
                             'MontContado' => $montContado,
                             'MontTotalCredito' => $montTotalCred,
@@ -595,7 +595,7 @@ class CargaServiciosController extends Controller
                             'MontPeaje'=> $MontPeaje,
                             'MontEspera' => $MontEspera,
                             'TiempoEspera' => $TiempoEspera,
-                            'FechaRegistroServicio' => '',
+                            'FechaRegistroServicio' => $FechaRegistroServicio,
                             'HoraLlegadaVehiculo'=> $HoraLlegadaVehiculo,
                             'HoraInicioServicio'=> $HoraInicioServicio,
                             'HoraFinServicio' => $HoraFinServicio,
